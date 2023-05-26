ee0a1c88d81df5c6315b183f95462f0943fc4bb5
psunderr <psunderr@gmail.com>
2023-05-25 18:58:05 -0700

    Update API to expose phase and PWM

--- src/api/OscillatorNode.js
+++ src/api/OscillatorNode.js
@@ -11,6 +11,8 @@ class OscillatorNode extends AudioScheduledSourceNode {
     this._impl = new impl.OscillatorNode(context._impl, opts);
     this._impl.$frequency = new AudioParam(context, this._impl.getFrequency());
     this._impl.$detune = new AudioParam(context, this._impl.getDetune());
+    this._impl.$phaseShift = new AudioParam(context, this._impl.getPhaseShift());
+    this._impl.$pulseWidth = new AudioParam(context, this._impl.getPulseWidth());
     this._impl.$onended = null;
 
     if (opts && opts.periodicWave) {
@@ -34,8 +36,20 @@ class OscillatorNode extends AudioScheduledSourceNode {
     return this._impl.$detune;
   }
 
+  get phaseShift() {
+    return this._impl.$phaseShift;
+  }
+
+  get pulseWidth() {
+    return this._impl.$pulseWidth;
+  }
+
   setPeriodicWave(periodicWave) {
     this._impl.setPeriodicWave(periodicWave);
+  }
+
+  phaseLockTo(oscillator) {
+    this._impl.phaseLockTo(oscillator._impl);
   }
 }
 
